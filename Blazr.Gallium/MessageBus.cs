fa0aba319e75db974ccbbcccd0f1702dfde927d2
Shaun Curtis <shaun@coldelm.com>
2024-12-12 22:34:12 +0000

    Update

--- Blazr.Gallium/MessageBus.cs
+++ Blazr.Gallium/MessageBus.cs
@@ -3,6 +3,7 @@
 /// License: Use And Donate
 /// If you use it, donate something to a charity somewhere
 /// ============================================================
+using System.Collections.Concurrent;
 
 namespace Blazr.Gallium;
 
@@ -13,40 +14,29 @@ public class MessageBus : IMessageBus, IDisposable
 
     public void Subscribe<T>(Action<object> callback) where T : IMessage
     {
-        _semaphore.Wait();
-
         var subscription = new Subscription(typeof(T), callback);
 
         if (!_subscriptions.Contains(subscription))
             _subscriptions.Add(subscription);
-
-        _semaphore.Release();
     }
 
     public void Publish<T>(T message) where T : IMessage
     {
         ArgumentNullException.ThrowIfNull(message);
 
-        _semaphore.Wait();
-
-        var handlers = _subscriptions.Where(item => item.SubscriptionType == typeof(T));
+        var handlers = _subscriptions.Where(item => item.SubscriptionType == typeof(T)).ToList();
 
         foreach (var handler in handlers)
             ThreadPool.QueueUserWorkItem((state) => handler.SubscriptionAction.Invoke(message));
 
-        _semaphore.Release();
     }
 
     public void UnSubscribe<T>(Action<object> callback) where T : IMessage
     {
-        _semaphore.Wait();
-
         var subscription = new Subscription(typeof(T), callback);
 
         if (_subscriptions.Contains(subscription))
             _subscriptions.Remove(subscription);
-
-        _semaphore.Release();
     }
 
     public void Dispose()
